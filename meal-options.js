abd8dbcbb227b4a15b00a5f7f8c9525ce5b74f5d
itsjoesullivan <itsjoesullivan@gmail.com>
2015-06-29 18:26:03 -0400

    let meal options be a file; remove jquery

---
+++ meal-options.js
@@ -0,0 +1,35 @@
+window.options = [
+  {
+    name: "Chipotle",
+    characteristics: {
+      spicy: 6,
+      meaty: 6,
+      wet: 5
+    }
+  },
+  {
+    name: "Chop't",
+    characteristics: {
+      meaty: 3,
+      spicy: 4,
+      wet: 7
+    }
+  },
+  {
+    name: "Shake Shack",
+    characteristics: {
+      meaty: 9,
+      spicy: 1,
+      wet: 6
+    }
+  }
+  /*
+  {
+    name: "Iris Cafe",
+    characteristics: {
+      freshness: 6,
+      creativity: 4
+    }
+  }
+  */
+];
