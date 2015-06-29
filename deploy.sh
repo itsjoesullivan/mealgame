5fe5e9bbb68321a592e6f633d8445429d9aabff7
itsjoesullivan <itsjoesullivan@gmail.com>
2015-06-29 19:24:03 -0400

    try this

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
-./build.sh
+browserify -t reactify script.jsx > build/_script.js
 git checkout gh-pages
+mv build/_script.js build/script.js
 git add build/script.js
 git commit -m "new build"
 git push origin gh-pages
