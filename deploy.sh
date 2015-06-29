228c8b35c27291d24d3b6097b32d33bdc7b5d69b
itsjoesullivan <itsjoesullivan@gmail.com>
2015-06-29 19:59:04 -0400

    comments

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,20 @@
+# Deploy to mealgame.biz
+
+# Build to tmp file, then overwrite current build/script.js
 mkdir -p build
 browserify -t reactify script.jsx > build/_script.js
 git checkout gh-pages
 mv build/_script.js build/script.js
+git add build/script.js
+
+# Static assets need to be moved like so:
 git checkout master index.html
 git checkout master style.css
 git checkout master meal-options.js
-git add build/script.js
-git commit -m "new build"
+
+# Commit and push changes
+git commit -m "deploy"
 git push origin gh-pages
+
+# Return to master
 git checkout master
