62128f6f412a693894561666929c7311ceb15e7e
itsjoesullivan <itsjoesullivan@gmail.com>
2015-06-29 19:38:19 -0400

    more files

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
 browserify -t reactify script.jsx > build/_script.js
 git checkout gh-pages
 mv build/_script.js build/script.js
+git checkout master index.html
+git checkout master style.css
+git checkout master meal-options.js
 git add build/script.js
 git commit -m "new build"
 git push origin gh-pages
