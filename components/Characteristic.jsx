84f6b30f9cb8a4872075c02422c58fd61b43396b
itsjoesullivan <itsjoesullivan@gmail.com>
2015-06-29 20:21:19 -0400

    formatting

--- components/Characteristic.jsx
+++ components/Characteristic.jsx
@@ -1,12 +1,10 @@
 module.exports = React.createClass({
   render: function() {
-    return (
-      <div>
-        <label>{this.props.name}</label>
-        <br />
-        <input type="range" min="0" max="10" step="1" onChange={this.onChange} />
-      </div>
-    );
+    return <div>
+      <label>{this.props.name}</label>
+      <br />
+      <input type="range" min="0" max="10" step="1" onChange={this.onChange} />
+    </div>
   },
   onChange: function(e) {
     preferences[this.props.name] = parseInt(e.target.value);
