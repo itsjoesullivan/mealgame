9982ac2a0d1afec9fb6606df31b7c113826574c9
itsjoesullivan <itsjoesullivan@gmail.com>
2015-06-29 20:20:42 -0400

    formatting

--- components/Guess.jsx
+++ components/Guess.jsx
@@ -1,17 +1,13 @@
 module.exports = React.createClass({
   render: function() {
     if (typeof this.props.score === 'number') {
-      return (
-        <div>
-          You should eat: {this.props.name}
-        </div>
-      );
+      return <div>
+        You should eat: {this.props.name}
+      </div>
     } else {
-      return (
-        <div>
-          No guess yet!
-        </div>
-      );
+      return <div>
+        No guess yet!
+      </div>
     }
   }
 });
