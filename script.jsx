03043580c64c907c743521c132e82a08d94b4ef8
itsjoesullivan <itsjoesullivan@gmail.com>
2015-07-03 15:38:09 -0400

    include meal-options in script

--- script.jsx
+++ script.jsx
@@ -3,6 +3,8 @@ window.preferences = {};
 // Something to trigger/listen to
 window.emitter = new EventEmitter();
 
+var restaurants = require('./meal-options');
+
 var makeGuess = require('./lib/makeGuess');
 var getCharacteristicsList = require('./lib/getCharacteristicsList');
 var Game = require('./components/Game.jsx');
